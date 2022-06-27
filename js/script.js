a33931bc137d188eafd78c8ee4672903296f19ef
github_DASorescu <srsdln97@gmail.com>
2022-06-27 18:07:25 +0200

    milestone-2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("ciao")
+// creiamo un array che contanga tutte le immagini 
+
+
+
+const sources = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg","img/06.jpg","img/07.jpg","img/08.jpg","img/09.jpg","img/10.jpg"];
+console.log(sources)
+
+// definire la variabile per contenere le immagini 
+let slider = "<ul>"
+
+// cislo for
+for(let i = 0; i < sources.length ; i++){
+    slider += `<li><img src="${sources[i]}"></li>`
+}
+
+const arrayContainer = document.getElementById("img-container");
+
+
+slider += "</ul>";
+console.log(slider);
+arrayContainer.innerHTML = slider;
